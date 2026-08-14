cf6a97f5d7471dc391b4b189081aec773f169e69
iaparamayores <iaparamayoresapp@gmail.com>
2026-08-14 11:26:37 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'boveda-v1';
+const ASSETS = [
+  './index.html'
+];
+
+// Instalar la app en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Servir la app desde caché (sin internet)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
